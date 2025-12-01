36404aa395b0c70657e9e1c3a23163ed969b2a46
HolyLight34 <349490516@qq.com>
2025-12-01 23:02:43 +0800

    创建角色状态机

---
+++ character/scripts/character.gd
@@ -0,0 +1,48 @@
+class_name Character
+extends CharacterBody2D
+var states: Array[State]
+var current_state: State:
+	get: return states.front()
+var previous_state: State:
+	get: return states[1]
+
+var direction: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	pass
+func _unhandled_input(event: InputEvent) -> void:
+	change_state(current_state.handle_input(event))
+	pass	
+func _process(delta: float) -> void:
+	change_state(current_state.process(delta))
+	pass
+	
+func _physics_process(delta: float) -> void:
+	change_state(current_state.physics_process(delta))
+	pass
+
+func initialize_states() -> void:
+	states = []
+	for c in $State.get_children():
+		states.append(c)
+		c.character = self
+	if states.size() == 0:
+		return
+	for state in states:
+		state.init()	
+	change_state(current_state)
+	pass
+	
+func change_state(new_state: State) -> void:
+	if new_state == null:
+		return
+	elif new_state == current_state:
+		return
+	if current_state:
+		current_state.exit()
+	states.push_front(new_state)
+	current_state.enter()
+	states.resize(3)			
+	pass
+	
+	
---
+++ character/scripts/state_walk.gd
@@ -0,0 +1,26 @@
+@icon("res://character/state.svg")
+class_name StateWalk
+extends Node
+
+var character: Character
+var nex_state: State
+
+func init() -> void:
+	pass
+	
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+
+func handle_input(_event: InputEvent) -> State:
+	return nex_state
+	
+func process(_delta: float) -> State:
+	return nex_state
+	
+func physics_process(_delta: float) -> State:
+	return nex_state
+	
+	
