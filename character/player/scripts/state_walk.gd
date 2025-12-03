1bbecbce6bf689d8338d5cabbdff238999ea1153
HolyLight34 <349490516@qq.com>
2025-12-03 23:46:30 +0800

    实现每次按走动键切换腿

--- character/player/scripts/state_walk.gd
+++ character/player/scripts/state_walk.gd
@@ -1,12 +1,16 @@
 @icon("res://character/state.svg")
 class_name StateWalk
 extends State
-
+var walk_frame: int = 0 # 动画起始帧
+var walk_frame_count: int = 2 # 腿部动作的帧数
+var frame_duration: float = 0.2  # 每帧持续时间
 func init() -> void:
 	pass
 	
 func enter() -> void:
-	
+	walk_frame = (walk_frame + 1) % walk_frame_count  # 循环计数 每次切换帧
+	player.animation_player.play("walk")
+	player.animation_player.seek(walk_frame * frame_duration, true)
 	pass
 	
 func exit() -> void:
