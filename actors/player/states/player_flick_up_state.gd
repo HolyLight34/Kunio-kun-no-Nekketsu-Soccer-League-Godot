4a0c4dc545079a1083808b041bd3d09b80309c95
HolyLight34 <349490516@qq.com>
2026-09-13 23:27:20 +0800

    feat: 实现原地挑球基础逻辑

---
+++ actors/player/states/player_flick_up_state.gd
@@ -0,0 +1,27 @@
+extends PlayerState
+
+func enter(_data) -> void:
+	player.player_horizontal_movement.stop_immediately()
+	anim.play(anim_name)
+	player.carried_ball.ball_z_movement.launch(8)
+	player.release_ball()
+	await player.step_animation_component.animation_finished
+	change_state(State.IDLE)
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func process(_delta: float) -> void:
+	pass
+
+
+func handle_intent(_intent: int, _delta: float) -> void:
+	pass
+
+
+func physics_process(_delta: float) -> void:
+
+	pass
